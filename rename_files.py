10f85ce2c8cd445feba5d38bfb3e3923b795a4d9
Igor Makienko <imaki@softserveinc.com>
2019-05-15 12:33:00 +0300

    modified function, added an argument

--- rename_files.py
+++ rename_files.py
@@ -1,17 +1,15 @@
 import os
 
-folder = '/Users/imaki/DubaiPictures/' # Destination directory
-
-def rename_files():
+def rename_files(folder_path):
     i = 1 # From what number starts to count in the loop
 
-# I use sorted func, because listdir doesn't sort the files
-    for file in sorted(os.listdir(folder)):
-        dst = "dubai" + str(i) + ".jpg" # Set name and extansion
-        src = folder + file
-        dst = folder + dst
+    # I use sorted func, because listdir doesn't sort the files
+    for file in sorted(os.listdir(folder_path)):
+        dst = "pict" + str(i) + ".jpeg" # Set name and extension
+        src = folder_path + file
+        dst = folder_path + dst
 
         os.rename(src, dst) # rename file
         i += 1
 
-rename_files()
+rename_files('/Users/imaki/Documents/test/') # Argument - path to the files
