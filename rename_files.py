bacf167cd1e8c96317866c038c32cce2e4a3839d
Igor Makienko <imaki@softserveinc.com>
2019-05-17 10:43:37 +0300

    Changed file to file_name

--- rename_files.py
+++ rename_files.py
@@ -4,13 +4,14 @@ def rename_files(folder_path):
     i = 1 # From what number starts to count in the loop
 
     # I use sorted func, because listdir doesn't sort the files
-    for file in sorted(os.listdir(folder_path)):
-        src = folder_path + file
+    for file_name in sorted(os.listdir(folder_path)):
+        src = folder_path + file_name
+
         if os.stat(src).st_size > 5242880:
             dst = "sportcar" + str(i) + ".mp4" # Set name and extension
         else:
             dst = "sportcar" + str(i) + ".jpg"
-            
+
         dst = folder_path + dst
 
         os.rename(src, dst) # rename file
