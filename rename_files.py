c08ec13792fb26029b9ecf939251675c2bbdc927
Igor Makienko <imaki@softserveinc.com>
2019-05-17 16:42:57 +0300

    Added sort by date_of_create

--- rename_files.py
+++ rename_files.py
@@ -1,20 +1,23 @@
 import os
 
 def rename_files(folder_path):
-    i = 1 # From what number starts to count in the loop
+    n = 1 # From what number starts to count in the loop
 
-    # I use sorted func, because listdir doesn't sort the files
-    for file_name in sorted(os.listdir(folder_path)):
-        src = folder_path + file_name
+    file_list = os.listdir(folder_path)
+    full_list = [os.path.join(folder_path, i) for i in file_list]
+    time_sorted_list = sorted(full_list, key=os.path.getctime)
 
-        if os.stat(src).st_size > 5242880:
-            dst = "sportcar" + str(i) + ".mp4" # Set name and extension
+    for file_name in time_sorted_list:
+        #src = folder_path + file_name
+
+        if os.stat(file_name).st_size > 5242880: # More than 5Mb
+            dst = "sp" + str(n) + ".mp4" # Set name and extension
         else:
-            dst = "sportcar" + str(i) + ".jpg"
+            dst = "sp" + str(n) + ".jpg"
 
         dst = folder_path + dst
 
-        os.rename(src, dst) # rename file
-        i += 1
+        os.rename(file_name, dst) # rename file
+        n += 1
 
-rename_files('/Users/imaki/Documents/test/') # Argument - path to the files
+rename_files('C:\\Users\\Administrator\\Downloads\\test\\')
